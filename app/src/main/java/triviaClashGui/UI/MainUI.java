ec06dbcd043e5f98f18baa942eca33d88a8b4364
programmer <leolegendnarasimha@gmail.com>
2025-01-03 21:53:14 +0530

    Code Improvements, UI improvements
    - Removed unwanted imports
    - Added borders around buttons
    - Added an additional panel to display next screen menu items (A few
      more buttons).
    - Added an holder with card layout, which helps in navigation between
      the menu panels (works with play button functionality currently).
    - holder panel is added into mainPanelUI instead of a single panel to keep the two menu panels in a stack .
    - play button is functional now which switchs to next menu items list
      panel on the holder panel
    - Formatting fixes

--- app/src/main/java/triviaClashGui/UI/MainUI.java
+++ app/src/main/java/triviaClashGui/UI/MainUI.java
@@ -2,29 +2,40 @@ package triviaClashGui.UI;
 
 import triviaClashGui.util.UIConstructor;
 
-import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JButton; 
+import javax.swing.border.EmptyBorder;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Insets;
 import java.awt.Component;
 import java.awt.CardLayout;
 import java.awt.GridLayout;
 import java.awt.Color;
 import java.awt.BorderLayout;
 import java.awt.Container;
-import java.awt.Dimension;
 import java.awt.Font; 
 
 public class MainUI implements UIConstructor {
-    Container mainContainer;
-    JPanel titlePanel, buttonPanel, mainUIPanel;
-    JLabel titleLabel;
-    JButton playButton, exitButton;
-    GridLayout menuButtonLayout = new GridLayout(2, 1);
+    private Container mainContainer;
+    private JPanel mainUIPanel;
+    private JPanel titlePanel;
+    private JPanel buttonPanel, additionalButtonPanel;
+    private JPanel holderPanel;
+    
+    private JLabel titleLabel;
+    private JButton playButton, exitButton;
+    private JButton singlePlayerButton, multiPlayerButton, helpButton, backButton;
 
-    Font titleFont = new Font("monospaced", Font.BOLD, 50);
-    Font buttonFont = new Font("serif", Font.BOLD, 20);
+    private GridLayout menuButtonLayout = new GridLayout(4, 1);
+    private GridLayout additionalMenuButtonLayout = new GridLayout(4, 1);
+    private GridLayout mainUIlayout = new GridLayout(2, 1);
+    private CardLayout cl;
+
+    private Font titleFont = new Font("monospaced", Font.BOLD, 50);
+    private Font buttonFont = new Font("serif", Font.BOLD, 20);    
     
     @Override
     public void construct() {
@@ -39,8 +50,14 @@ public class MainUI implements UIConstructor {
 
     @Override 
     public void initializeWindow() {
-        mainUIPanel = new JPanel(null);
-        
+        mainUIlayout.setVgap(10);
+        mainUIlayout.setHgap(50);
+        mainUIPanel = new JPanel(mainUIlayout);
+       
+        holderPanel = new JPanel();
+        holderPanel.setLayout(new CardLayout());
+
+        cl = (CardLayout)holderPanel.getLayout();
         mainUIPanel.setBackground(Color.BLACK);
     } 
 
@@ -48,32 +65,29 @@ public class MainUI implements UIConstructor {
     public void initializeComponents() {
         
         // Title label and panel configuration
-        titlePanel = new JPanel(new BorderLayout());
+        titlePanel = new JPanel(new BorderLayout(50, 50));
         titleLabel = new JLabel("Trivia Clash!", JLabel.CENTER);
         titleLabel.setFont(titleFont);
-        titleLabel.setBackground(Color.BLACK);
-        titleLabel.setForeground(Color.WHITE);
-
-        titlePanel.add(titleLabel);
-        titlePanel.setBackground(Color.RED); // testing 
-        titlePanel.setBounds(100, 90, 400, 50);
+        titleLabel.setForeground(Color.ORANGE);
         titlePanel.add(titleLabel);
         titlePanel.setBackground(Color.BLACK);
-        titlePanel.setForeground(Color.WHITE);
-
+        titlePanel.setBorder(new EmptyBorder(new Insets(10, 10, 10, 10))); 
         // Start button configuration 
-        menuButtonLayout.setVgap(3);
+        menuButtonLayout.setVgap(5);
+        menuButtonLayout.setHgap(60);
+        
         buttonPanel = new JPanel(menuButtonLayout);
         buttonPanel.setBackground(Color.BLACK);
-        buttonPanel.setBackground(Color.BLACK); // testing
-        buttonPanel.setBounds(200, 230, 200, 80);
+        buttonPanel.setSize(200, 80);
         buttonPanel.setForeground(Color.WHITE);
-
+        buttonPanel.setBorder(new EmptyBorder(new Insets(50, 50, 50, 50)));
+       
         playButton = new JButton("PLAY");
         playButton.setFont(buttonFont);
         playButton.setBackground(Color.BLACK);
         playButton.setForeground(Color.WHITE);
         playButton.setFocusPainted(false);
+        setPlayButtonAction();
 
         exitButton = new JButton("EXIT");
         exitButton.setFont(buttonFont);
@@ -83,13 +97,65 @@ public class MainUI implements UIConstructor {
 
         buttonPanel.add(playButton);
         buttonPanel.add(exitButton);
+        
+        additionalMenuButtonLayout.setHgap(60);
+        additionalMenuButtonLayout.setVgap(5);
 
-        mainUIPanel.add(buttonPanel);
+        additionalButtonPanel = new JPanel(additionalMenuButtonLayout);
+        additionalButtonPanel.setBackground(Color.BLACK);
+        additionalButtonPanel.setSize(200, 80);
+        additionalButtonPanel.setForeground(Color.WHITE);
+        additionalButtonPanel.setBorder(new EmptyBorder(new Insets(50, 50, 50, 50)));
+
+        
+        singlePlayerButton = new JButton("SINGLE PLAYER");
+        singlePlayerButton.setFont(buttonFont);
+        singlePlayerButton.setBackground(Color.BLACK);
+        singlePlayerButton.setForeground(Color.WHITE);
+        singlePlayerButton.setFocusPainted(false);
+
+        multiPlayerButton = new JButton("MULTI PLAYER");
+        multiPlayerButton.setFont(buttonFont);
+        multiPlayerButton.setBackground(Color.BLACK);
+        multiPlayerButton.setForeground(Color.WHITE);
+        multiPlayerButton.setFocusPainted(false);
+
+        helpButton = new JButton("HELP");
+        helpButton.setFont(buttonFont);
+        helpButton.setBackground(Color.BLACK);
+        helpButton.setForeground(Color.WHITE);
+        helpButton.setFocusPainted(false);
+
+        backButton = new JButton("BACK");
+        backButton.setFont(buttonFont);
+        backButton.setBackground(Color.BLACK);
+        backButton.setForeground(Color.BLUE);
+        backButton.setFocusPainted(false);
+        
+        additionalButtonPanel.add(singlePlayerButton);
+        additionalButtonPanel.add(multiPlayerButton);
+        additionalButtonPanel.add(helpButton);
+        additionalButtonPanel.add(backButton);
+       
+        holderPanel.add(buttonPanel);
+        holderPanel.add(additionalButtonPanel);
+
         mainUIPanel.add(titlePanel);
+        mainUIPanel.add(holderPanel); 
     }
 
     @Override 
     public Component getPanel() {
         return mainUIPanel;
+    }
+
+    private void setPlayButtonAction() {
+        playButton.addActionListener(
+            new ActionListener(){
+                public void actionPerformed(ActionEvent event) {
+                    cl.next(holderPanel);
+                }
+            }         
+        ); 
     }
 }
